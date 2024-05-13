baba31d2cd9476d60bd3f81002581d354253cfc3
Vishwasp123 <vishawasparashar111@gmail.com>
2024-05-13 14:39:09 +0530

    add sub_category_rspec

--- spec/models/sub_category_spec.rb
+++ spec/models/sub_category_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe SubCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) {create(:category)}
+  let(:valid_attributes) {{name:"Electronics Accessories", category: category}}
+
+  describe "asscociation" do 
+    it {should belong_to(:category)}
+  end
+  describe "validates" do 
+    it {should validate_presence_of(:name)}
+  end
+  describe ".ransack_attributes" do 
+    it ".ransack attributes RSpec" do 
+      ransack_attributes = ["category_id", "created_at", "id", "id_value", "name", "updated_at"]
+      expect(SubCategory.ransackable_attributes).to match_array(ransack_attributes)
+    end
+  end
 end
