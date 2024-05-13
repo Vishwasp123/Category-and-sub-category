a95ebc63abe7d405fd91876cafad773b4f125f61
Vishwasp123 <vishawasparashar111@gmail.com>
2024-05-13 14:49:52 +0530

    Add textcases for admin_user

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe AdminUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:admin_user) {create(:admin_user)}
+  describe "validations" do 
+    it {should validate_presence_of(:email)}
+    it {should validate_presence_of(:password)}
+  end
+  describe "ransack_attributes" do 
+    it "ransack_attributes rspec" do 
+      admin_user_ransack =     ["created_at", "email", "encrypted_password", "id", "id_value", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
+      expect(AdminUser.ransackable_attributes).to match_array(admin_user_ransack )
+    end
+  end
 end
